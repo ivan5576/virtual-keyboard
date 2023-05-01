eb419bc4b97b593277c105c5f788b94ec898c84e
Ivan Lebedev <vaanya@ya.ru>
2023-05-01 21:15:28 +0300

    refactor: style CSS to buttons and add keys to arrs

--- app.js
+++ app.js
@@ -30,266 +30,330 @@ function addKeyboard() {
     {
       key: '`',
       code: 'Backquote',
+      EN: '`',
     },
     {
       key: '1',
       code: 'Digit1',
+      EN: '1',
     },
     {
       key: '2',
       code: 'Digit2',
+      EN: '2',
     },
     {
       key: '3',
       code: 'Digit3',
+      EN: '3',
     },
     {
       key: '4',
       code: 'Digit4',
+      EN: '4',
     },
     {
       key: '5',
       code: 'Digit5',
+      EN: '5',
     },
     {
       key: '6',
       code: 'Digit6',
+      EN: '6',
     },
     {
       key: '7',
       code: 'Digit7',
+      EN: '7',
     },
     {
       key: '8',
       code: 'Digit8',
+      EN: '8',
     },
     {
       key: '9',
       code: 'Digit9',
+      EN: '9',
     },
     {
       key: '0',
       code: 'Digit0',
+      EN: '0',
     },
     {
       key: '-',
       code: 'Minus',
+      EN: '-',
     },
     {
       key: '=',
       code: 'Equal',
+      EN: '=',
     },
     {
       key: 'Backspace',
       code: 'Backspace',
+      EN: 'Backspace',
     },
   ];
   const row2 = [
     {
       key: 'Tab',
       code: 'Tab',
+      EN: 'Tab',
     },
     {
       key: 'q',
       code: 'KeyQ',
+      EN: 'q',
     },
     {
       key: 'w',
       code: 'KeyW',
+      EN: 'w',
     },
     {
       key: 'e',
       code: 'KeyE',
+      EN: 'e',
     },
     {
       key: 'r',
       code: 'KeyR',
+      EN: 'r',
     },
     {
       key: 't',
       code: 'KeyT',
+      EN: 't',
     },
     {
       key: 'y',
       code: 'KeyY',
+      EN: 'y',
     },
     {
       key: 'u',
       code: 'KeyU',
+      EN: 'u',
     },
     {
       key: 'i',
       code: 'KeyI',
+      EN: 'i',
     },
     {
       key: 'o',
       code: 'KeyO',
+      EN: 'o',
     },
     {
       key: 'p',
       code: 'KeyP',
+      EN: 'p',
     },
     {
       key: '[',
       code: 'BracketLeft',
+      EN: '[',
     },
     {
       key: ']',
       code: 'BracketRight',
+      EN: ']',
     },
     {
       key: '\\',
       code: 'Backslash',
+      EN: '\\',
     },
     {
       key: 'Delete',
       code: 'Delete',
+      EN: 'Del',
     },
   ];
   const row3 = [
     {
       key: 'CapsLock',
       code: 'CapsLock',
+      EN: 'CapsLock',
     },
     {
       key: 'a',
       code: 'KeyA',
+      EN: 'a',
     },
     {
       key: 's',
       code: 'KeyS',
+      EN: 's',
     },
     {
       key: 'd',
       code: 'KeyD',
+      EN: 'd',
     },
     {
       key: 'f',
       code: 'KeyF',
+      EN: 'f',
     },
     {
       key: 'g',
       code: 'KeyG',
+      EN: 'g',
     },
     {
       key: 'h',
       code: 'KeyH',
+      EN: 'h',
     },
     {
       key: 'j',
       code: 'KeyJ',
+      EN: 'j',
     },
     {
       key: 'k',
       code: 'KeyK',
+      EN: 'k',
     },
     {
       key: 'l',
       code: 'KeyL',
+      EN: 'l',
     },
     {
       key: ';',
       code: 'Semicolon',
+      EN: ';',
     },
     {
       key: "'",
       code: 'Quote',
+      EN: "'",
     },
     {
       key: 'Enter',
       code: 'Enter',
+      EN: 'Enter',
     },
   ];
   const row4 = [
     {
       key: 'Shift',
       code: 'ShiftLeft',
+      EN: 'Shift',
     },
     {
       key: 'z',
       code: 'KeyZ',
+      EN: 'z',
     },
     {
       key: 'x',
       code: 'KeyX',
+      EN: 'x',
     },
     {
       key: 'c',
       code: 'KeyC',
+      EN: 'c',
     },
     {
       key: 'v',
       code: 'KeyV',
+      EN: 'v',
     },
     {
       key: 'b',
       code: 'KeyB',
+      EN: 'b',
     },
     {
       key: 'n',
       code: 'KeyN',
+      EN: 'n',
     },
     {
       key: 'm',
       code: 'KeyM',
+      EN: 'm',
     },
     {
       key: ',',
       code: 'Comma',
+      EN: ',',
     },
     {
       key: '.',
       code: 'Period',
+      EN: '.',
     },
     {
       key: '/',
       code: 'Slash',
+      EN: '/',
     },
     {
       key: 'ArrowUp',
       code: 'ArrowUp',
+      EN: '&uarr;',
     },
     {
       key: 'Shift',
       code: 'ShiftRight',
+      EN: 'Shift',
     },
   ];
   const row5 = [
     {
       key: 'Control',
       code: 'ControlLeft',
+      EN: 'Ctrl',
     },
     {
       key: 'Meta',
       code: 'MetaLeft',
+      EN: 'Win',
     },
     {
       key: 'Alt',
       code: 'AltLeft',
+      EN: 'Alt',
     },
     {
       key: ' ',
       code: 'Space',
+      EN: ' ',
     },
     {
       key: 'Alt',
       code: 'AltRight',
+      EN: 'Alt',
     },
     {
       key: 'ArrowLeft',
       code: 'ArrowLeft',
+      EN: '&larr;',
     },
     {
       key: 'ArrowDown',
       code: 'ArrowDown',
+      EN: '&darr;',
     },
     {
       key: 'ArrowRight',
       code: 'ArrowRight',
+      EN: '&rarr;',
     },
     {
       key: 'Control',
       code: 'ControlRight',
+      EN: 'Ctrl',
     },
   ];
 
@@ -297,7 +361,7 @@ function addKeyboard() {
     let char = '';
 
     for (let i = 0; i < row.length; i += 1) {
-      char += `<div class="char ${row[i].code}"><p class="char__name">${row[i].key}</p></div>`;
+      char += `<div class="char ${row[i].code}"><p class="char__name">${row[i].EN}</p></div>`;
     }
     document.querySelectorAll('.keyboard__row')[rowNum].innerHTML = char;
   }
@@ -306,6 +370,11 @@ function addKeyboard() {
   addButton(row3, 2);
   addButton(row4, 3);
   addButton(row5, 4);
+
+  // for (let i = 0; i < row5.length; i++) {
+  //   row5[i].EN = `${row5[i].key}`;
+  // }
+  // console.log(row5);
 }
 
 addKeyboard();
